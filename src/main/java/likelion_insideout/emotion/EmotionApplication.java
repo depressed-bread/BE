7f3fab78df3316c51839a040b3bbcf870a1de25e
hanwldn76 <hanwldn76@gmail.com>
2024-07-11 20:53:08 +0900

    init

---
+++ src/main/java/likelion_insideout/emotion/EmotionApplication.java
@@ -0,0 +1,13 @@
+package likelion_insideout.emotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion_insideout/emotion/EmotionApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion_insideout.emotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
