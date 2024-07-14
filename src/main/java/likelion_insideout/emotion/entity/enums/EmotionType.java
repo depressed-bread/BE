b23920e5c9623d62a65d70be46eb78d1f5f22430
dbqudgns <dbqudgns4632@gmail.com>
2024-07-14 21:32:10 +0900

    Project_Structure_Final

--- src/main/java/likelion_insideout/emotion/entity/enums/EmotionType.java
+++ src/main/java/likelion_insideout/emotion/entity/enums/EmotionType.java
@@ -1,4 +1,21 @@
 package likelion_insideout.emotion.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum EmotionType {
+
+    PROUD("뿌듯"),
+    JOY("기쁨"),
+    SAD("슬픔"),
+    DEPRESSION("우울"),
+    ANXIETY("불안"),
+    THRILL("설렘"),
+    PANIC("당황"),
+    ANGRY("화남");
+
+    //감정을 저장할 필드(설명)
+    private final String description;
 }
